b3d2dfca9e5c9879070531a6e66080dde249cb30
Alexandre Quitério <a.quiterio@karma-consulting.pt>
2013-05-03 17:41:38 +0100

    update rails admin configuration file on the generator engine

--- lib/generators/templates/rails_admin.rb
+++ lib/generators/templates/rails_admin.rb
@@ -1,17 +1,8 @@
 RailsAdmin.config do |c|
-  c.model 'Survey::Answer' do
-    visible false
-  end
-
-  c.model 'Survey::Option' do
-    visible false
-  end
-
-  c.model 'Survey::Attempt' do
-    visible false
-  end
-
-  c.model 'Survey::Question' do
-    visible false
-  end
+  c.excluded_models = [
+    Survey::Answer,
+    Survey::Option,
+    Survey::Attempt,
+    Survey::Question
+  ]
 end
