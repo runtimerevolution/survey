546103083577f609f41bd77ef859df4500ef1e42
dr-click <ragab.mostafa@gmail.com>
2013-09-04 09:44:54 +0200

    change :survey_tentatives to :survey_attempts

--- lib/survey/active_record.rb
+++ lib/survey/active_record.rb
@@ -4,10 +4,10 @@ module Survey
 
     module ClassMethods
       def has_surveys
-        has_many :survey_tentatives, as: :participant, :class_name => ::Survey::Attempt
+        has_many :survey_attempts, as: :participant, :class_name => ::Survey::Attempt
 
         define_method("for_survey") do |survey|
-          self.survey_tentatives.where(:survey_id => survey.id)
+          self.survey_attempts.where(:survey_id => survey.id)
         end
       end
     end
