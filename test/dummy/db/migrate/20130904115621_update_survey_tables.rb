f2ca38c383f4c4ff40eef3a8c4ce8f09b9304fb6
dr-click <ragab.mostafa@gmail.com>
2013-09-05 09:44:35 +0200

    update the description field to be text

--- test/dummy/db/migrate/20130904115621_update_survey_tables.rb
+++ test/dummy/db/migrate/20130904115621_update_survey_tables.rb
@@ -2,19 +2,19 @@ class UpdateSurveyTables < ActiveRecord::Migration
   def change
     #Survey Surveys table
     add_column :survey_surveys, :locale_name, :string
-    add_column :survey_surveys, :locale_description, :string
+    add_column :survey_surveys, :locale_description, :text
     
     #Survey Sections table
     add_column :survey_sections, :locale_head_number, :string
     add_column :survey_sections, :locale_name, :string
-    add_column :survey_sections, :locale_description, :string
-      
+    add_column :survey_sections, :locale_description, :text
+    
     #Survey Questions table
     add_column :survey_questions, :head_number, :string
-    add_column :survey_questions, :description, :string
+    add_column :survey_questions, :description, :text
     add_column :survey_questions, :locale_text, :string
     add_column :survey_questions, :locale_head_number, :string
-    add_column :survey_questions, :locale_description, :string
+    add_column :survey_questions, :locale_description, :text
     
     #Survey Options table
     add_column :survey_options, :locale_text, :string
