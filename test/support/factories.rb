966e3431af02e9107b53180dcd8a9b978fc71efa
Daniel M <d.magarreiro@runtime-revolution.com>
2015-05-15 14:50:08 +0100

    answer weight is now independent of answer correctness

--- test/support/factories.rb
+++ test/support/factories.rb
@@ -3,17 +3,24 @@
 # Create a Survey::Survey
 def create_survey(opts = {})
   Survey::Survey.create({
-    :name => ::Faker::Name.name,
-    :attempts_number => 3,
-    :description => ::Faker::Lorem.paragraph(1)
+    name: ::Faker::Name.name,
+    attempts_number: 3,
+    description: ::Faker::Lorem.paragraph(1)
   }.merge(opts))
 end
 
 # Create a Survey::Question
 def create_question(opts = {})
   Survey::Question.create({
-    :text =>  ::Faker::Lorem.paragraph(1),
-    :options_attributes => {:option => correct_option_attributes}
+    text:  ::Faker::Lorem.paragraph(1),
+  }.merge(opts))
+end
+
+# Create a Survey::Question with an option
+def create_question_with_option(opts = {})
+  Survey::Question.create({
+    text:  ::Faker::Lorem.paragraph(1),
+    options_attributes: {option: correct_option_attributes}
   }.merge(opts))
 end
 
@@ -28,28 +35,28 @@ def create_option(opts = {})
 end
 
 def option_attributes
-  { :text => ::Faker::Lorem.paragraph(1) }
+  { text: ::Faker::Lorem.paragraph(1) }
 end
 
 def correct_option_attributes
-  option_attributes.merge({:correct => true})
+  option_attributes.merge({correct: true})
 end
 
-def create_attempt(opts ={})
-  attempt = Survey::Attempt.create do |t|
-    t.survey = opts.fetch(:survey, nil)
-    t.participant = opts.fetch(:user, nil)
-    opts.fetch(:options, []).each do |option|
-        t.answers.new(:option => option, :question => option.question, :attempt => t)
+def create_attempt(user: nil, survey: nil, chosen_options: [])
+  Survey::Attempt.create do |t|
+    t.survey = survey
+    t.participant = user
+    chosen_options.each do |option|
+      t.answers.new(option: option, question: option.question, attempt: t)
     end
   end
 end
 
-def create_survey_with_questions(num)
+def create_survey_with_questions(num_questions = 4, num_options = 4)
   survey = create_survey
-  num.times do
+  num_questions.times do
     question = create_question
-    num.times do
+    num_options.times do
       question.options << create_option(correct_option_attributes)
     end
     survey.questions << question
@@ -57,35 +64,47 @@ def create_survey_with_questions(num)
   survey
 end
 
-def create_attempt_for(user, survey, opts = {})
-
-  if opts.fetch(:all, :wrong) == :right
-    correct_survey = survey.correct_options
-    create_attempt({  :options => correct_survey,
-                      :user => user,
-                      :survey => survey})
-  else
-    incorrect_survey = survey.correct_options
-    incorrect_survey.shift
-    create_attempt({  :options  => incorrect_survey,
-                      :user => user,
-                      :survey => survey})
+def create_survey_with_questions_and_scores(num_questions = 4, num_options = 4, weight: 10)
+  survey = create_survey
+  num_questions.times do
+    question = create_question
+    num_options.times do
+      question.options << create_option(weight: weight)
+    end
+    survey.questions << question
   end
+  survey
+end
+
+# Creates a user attempt. If all correct is true the user will choose all the correct options.
+# If if it is false, the user will choose all the incorrect options.
+def create_attempt_for user, survey, all_correct: false
+  chosen_options = all_correct ? survey.correct_options : survey.incorrect_options
+  create_attempt({  chosen_options: chosen_options,
+                    user: user,
+                    survey: survey})
+end
+
+def create_attempt_choosing_first_option user, survey
+  chosen_options = survey.questions.map { |q| q.options.first }
+  create_attempt({  chosen_options: chosen_options,
+                    user: user,
+                    survey: survey})
 end
 
 def create_answer(opts = {})
   survey = create_survey_with_questions(6)
   question = survey.questions.first
   option   = survey.questions.first.options.first
-  attempt = create_attempt(:user => create_user, :survey => survey)
-  Survey::Answer.create({:option => option, :attempt => attempt, :question => question}.merge(opts))
+  attempt = create_attempt(user: create_user, survey: survey)
+  Survey::Answer.create({option: option, attempt: attempt, question: question}.merge(opts))
 end
 
 # Dummy Model from Dummy Application
 def create_user
-  User.create(:name => Faker::Name.name)
+  User.create(name: Faker::Name.name)
 end
 
 def create_sti_user
-  StiUser.create(:name => Faker::Name.name)
+  StiUser.create(name: Faker::Name.name)
 end
--- test/survey_test.rb
+++ test/survey_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
 
-  test "should pass if user b was the more right answers" do
+  test "should pass if user b was more right answers" do
     user_a = create_user
     user_b = create_user
     survey = create_survey_with_questions(4)
 
     create_attempt_for(user_a, survey)
-    create_attempt_for(user_b, survey, :all => :right)
+    create_attempt_for(user_b, survey, all_correct: true)
 
     assert_not_equal user_b, participant_with_more_wrong_answers(survey)
     assert_equal user_b, participant_with_more_right_answers(survey)
   end
 
-  test "should pass if all the users has the same score" do
+  test "should pass if all the users have the same score" do
     user_a = create_user
     user_b = create_sti_user
     survey = create_survey_with_questions(4)
 
-    create_attempt_for(user_a, survey, :all => :right)
-    create_attempt_for(user_b, survey, :all => :right)
+    create_attempt_for(user_a, survey, all_correct: true)
+    create_attempt_for(user_b, survey, all_correct: true)
 
     assert_equal participant_score(user_a, survey),
                  participant_score(user_b, survey)
@@ -33,9 +33,9 @@ class SurveyTest < ActiveSupport::TestCase
     survey_b = create_survey_with_questions(8)
 
     create_attempt_for(user_a, survey_a)
-    create_attempt_for(user_b, survey_a, :all => :right)
+    create_attempt_for(user_b, survey_a, all_correct: true)
 
-    create_attempt_for(user_a, survey_b, :all => :right)
+    create_attempt_for(user_a, survey_b, all_correct: true)
     create_attempt_for(user_b, survey_b)
 
     assert_equal user_a, participant_with_more_right_answers(survey_b)
@@ -43,6 +43,20 @@ class SurveyTest < ActiveSupport::TestCase
 
     assert_equal user_a, participant_with_more_wrong_answers(survey_a)
     assert_equal user_b, participant_with_more_wrong_answers(survey_b)
+  end
+
+  test "check if the score is positive" do
+    number_of_questions = 4
+    number_of_options_per_question = 4
+    weight_of_options = 10
+
+    user = create_user
+    survey = create_survey_with_questions_and_scores(number_of_questions, number_of_options_per_question, weight: weight_of_options)
+
+    create_attempt_choosing_first_option(user, survey)
+    
+    assert (survey.attempts.first.score > 0)
+    assert_equal survey.attempts.first.score, (number_of_questions * weight_of_options)
   end
 
 end
